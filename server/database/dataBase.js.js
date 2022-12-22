eb622af3eb4f9bdd8f7c14d162e35f9e9563ed9a
patschwendeman <85462423+patschwendeman@users.noreply.github.com>
2022-12-22 11:03:50 +0100

    init

--- server/database/dataBase.js.js
+++ server/database/dataBase.js.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 //Connection to MongoDB Atlas Database Cluster
 const db = mongoose.connect(
-    "mongodb+srv://user:mongoPasswordDB@cluster0.0e13z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    "mongodb+srv://eap4d:m20biw0w@cluster0.uv4nr.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
